4ac2dbdd1f24997a59738c29b16384839311c9f6
David <david.tapiador@envato.com>
2016-03-07 15:19:12 +1100

    fix tests

--- spec/uploadable_spec.rb
+++ spec/uploadable_spec.rb
@@ -16,7 +16,7 @@ describe StaticSync::Uploadable do
   describe "#headers" do
 
     it "should make all files viewable by everyone" do
-      html_file.headers[:public].should be_true
+      html_file.headers[:public].should be true
     end
 
     it "should reduce storage costs for all files" do
@@ -70,7 +70,7 @@ describe StaticSync::Uploadable do
 
   describe "#cached?" do
     it "returns false by default" do
-      subject.cached?.should be_false
+      subject.cached?.should be false
     end
 
     context "when caching is enabled for html" do
@@ -80,7 +80,7 @@ describe StaticSync::Uploadable do
       end
 
       it "returns true for a html file" do
-        html_file.cached?.should be_true
+        html_file.cached?.should be true
       end
     end
   end
@@ -137,11 +137,11 @@ describe StaticSync::Uploadable do
   describe "#gzipped?" do
 
     xit "returns true for html files" do
-      html_file.gzipped?.should be_true
+      html_file.gzipped?.should be true
     end
 
     it "returns false for png files" do
-      png_file.gzipped?.should be_false
+      png_file.gzipped?.should be false
     end
 
   end
